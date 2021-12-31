a5eecef0893195452a91788ac48de24dad534077
Diego Granados <diego@codeando.edu.pe>
2021-12-31 11:39:06 -0400

    finalizado

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 
-console.log(raceNumber);
+const registeredEarly = false;
+
+const age = 19;
+
+if (age >= 18 && registeredEarly === true) {
+  raceNumber += 1000;
+  console.log('Estoy aqui')
+}
+
+if (age >= 18 && registeredEarly === true) {
+  console.log(`You'r race number is ${raceNumber} and you will race at 9:30 am`);
+} else if (age => 18 && registeredEarly === false) {
+  console.log(`You'r race number is ${raceNumber} and you will race at 11:00 am`);
+
+}
