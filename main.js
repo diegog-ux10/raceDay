6edc9d0ba168d347120604192b2a7ff83eca3a7d
Diego Granados <diego@codeando.edu.pe>
2021-12-31 12:45:18 -0400

    Se agrego condicion para menores de edad y se corrigio operadores  de comparacion

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 
-const registeredEarly = false;
+const registeredEarly = true;
 
 const age = 19;
 
-if (age >= 18 && registeredEarly === tgit rue) {
+if (age >= 18 && registeredEarly === true) {
   raceNumber += 1000;
-}
+};
 
 if (age >= 18 && registeredEarly === true) {
-  console.log(`You'r race number is ${raceNumber} and you will race at 9:30 am`);
-} else if (age => 18 && registeredEarly === false) {
-  console.log(`You'r race number is ${raceNumber} and you will race at 11:00 am`);
-
-}
+  console.log (`You'r race number is ${raceNumber} and you will race at 9:30 am`);
+} else if (age >= 18 && registeredEarly === false) {
+  console.log (`You'r race number is ${raceNumber} and you will race at 11:00 am`);
+  } else if (age < 18) {
+    console.log (`You'r race number is ${raceNumber} and you will race at 12:30 pm`);
+  }
